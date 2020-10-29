f4796fb3d23f86f3381f076291b14978364b584e
jmskeoch <jmskeoch@yahoo.com>
2020-10-29 17:15:19 +0000

    Implement student class

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,58 @@
 public class Student {
-    
+    //Instance vars
+    private String firstName;
+    private String lastName;
+    private int classYear;
+
+    //Default constructor
+    public Student() {
+        this.firstName = "John";
+        this.lastName = "Doe";
+        this.classYear = 2022;
+    }   
+
+    //Main constructor
+    public Student(String firstName, String lastName, int classYear) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.classYear = classYear;
+    }
+
+    //Random doSomething method
+    public void doSomething() {
+        System.out.println("I am studying!");
+    }
+
+    //toString method
+    @Override
+    public String toString() {
+        String newString = firstName + " , " + lastName + " , " + classYear;
+        return newString;
+    }
+
+    //Getters
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getClassYear() {
+        return this.classYear;
+    }
+
+    //Setters
+    public void setFirstName(String x) {
+        this.firstName = x;
+    }
+
+    public void setLastName(String x) {
+        this.lastName = x;
+    }
+
+    public void setClassYear(int x) {
+        this.classYear = x;
+    }
 }
--- src/main/java/StudentClient.java
+++ src/main/java/StudentClient.java
@@ -1,5 +1,14 @@
 public class StudentClient {
     public static void main(String[] args) {
-        
+        Student s1 = new Student();
+        Student s2 = new Student("Jason", "Skeoch", 2022);
+        s1.doSomething();
+        s2.doSomething();
+        System.out.println(s1.getFirstName());
+        System.out.println(s2.getLastName());
+        System.out.println(s1.getClassYear());
+        System.out.println(s2.getClassYear());
+        System.out.println(s1);
+        System.out.println(s2);
     }
 }
